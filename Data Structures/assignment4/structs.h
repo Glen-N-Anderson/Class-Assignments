382a36b95ad5950ce5f92b11789a8f2f30506758
Glen-N-Anderson <glen.nowel.anderson@gmail.com>
2017-01-05 13:55:44 -0800

    Add files via upload

---
+++ Data Structures/assignment4/structs.h
@@ -0,0 +1,11 @@
+/* You can modify the structure to store whatever you'd like in your BST */
+
+struct data {
+	int number;
+	char *name;
+};
+struct data2 {
+	float number;
+	char *name;
+	char label;
+};
